9100de5e03084cbbd1c92b58d3be9c562d51c960
vinozganic <jakov.vinozganic@fer.hr>
2024-10-23 22:46:35 +0200

    Update server port configuration

--- qr-api/src/main.ts
+++ qr-api/src/main.ts
@@ -23,6 +23,8 @@ async function bootstrap() {
     const document = SwaggerModule.createDocument(app, config);
     SwaggerModule.setup('api', app, document);
 
-    await app.listen(8001);
+    const port = process.env.PORT || 8001;
+
+    await app.listen(port);
 }
 bootstrap();
--- qr-webapp/src/server.ts
+++ qr-webapp/src/server.ts
@@ -1,7 +1,7 @@
 import app from './app';
 
-const PORT = process.env.PORT;
+const port = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
 });
